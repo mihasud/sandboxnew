9726911f9c4871c0510ae24e8e5c2614109a617f
Miha Sud <miha.sud@gmail.com>
2018-02-15 14:13:46 -0800

    Added couple of functions

--- src/main/java/com/mihasud/sandbox/MyFirstProgram.java
+++ src/main/java/com/mihasud/sandbox/MyFirstProgram.java
@@ -3,7 +3,27 @@ package com.mihasud.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    hello("World");
+    hello("user");
+    hello("Michael");
+
+    double l = 5;
+    System.out.println("Area of square with side " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Area of square with sides " + a + " and "+ b + " = " + area(5,3 ));
   }
 
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
