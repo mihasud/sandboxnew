225b033268b44a30f2382ce4d33ae84b2ae5aae6
Miha Sud <miha.sud@gmail.com>
2018-02-26 15:27:53 -0800

    New changes

--- src/main/java/com/mihasud/sandbox/MyFirstProgram.java
+++ src/main/java/com/mihasud/sandbox/MyFirstProgram.java
@@ -7,23 +7,15 @@ public class MyFirstProgram {
     hello("user");
     hello("Michael");
 
-    double l = 5;
-    System.out.println("Area of square with side " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Area of square with side " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Area of square with sides " + a + " and "+ b + " = " + area(5,3 ));
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Area of square with sides " + r.a + " and "+ r.b + " = " + r.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello " + somebody + "!");
   }
-
-  public static double area(double len) {
-    return len * len;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
-  }
+  
 }
